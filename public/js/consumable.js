74aeacb887d68607b084067943fee3979f065ec7
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-08-15 23:50:24 +0200

    feat: gestion du click sur le bouton sort-by sur toutes les pages

--- public/js/consumable.js
+++ public/js/consumable.js
@@ -1,9 +1,11 @@
 const consu = {
+
+    consumableCards: document.querySelectorAll('.consumable-card'),
     
     init() {
         consu.searchConsumables();
-        tool.listenToClickOnSortButton();
-        tool.closeSortOptions();
+        consu.listenToClickOnSortButton();
+        consu.closeSortOptions();
     },
 
     //---------------------------
@@ -11,23 +13,76 @@ const consu = {
     //---------------------------
 
     /**
-     * Listen to the text put in the search bar and filter the weapons including the text
+     * Listen to the text put in the search bar and filter the consumables including the text
      */
     searchConsumables() {
         const searchInput = document.querySelector('.search-bar');
-        const consumableCards = document.querySelectorAll('.consumable-card');
-    
         searchInput.addEventListener('input', () => {
             const searchText = searchInput.value.toLowerCase();
+            let resultFound = false;
     
-            consumableCards.forEach(card => {
+            consu.consumableCards.forEach(card => {
                 const consumableName = card.querySelector('.name').textContent.toLowerCase();
                 if (consumableName.includes(searchText)) {
-                    card.style.display = 'block';
+                    card.classList.remove('hidden');
+                    resultFound = true;
                 } else {
-                    card.style.display = 'none';
+                    card.classList.add('hidden');
                 }
             });
+
+            if(!resultFound) {
+                consu.createNoResultsFoundMessage(); 
+            } else {
+                consu.hideNoResultsFoundMessage();
+            }
+        });
+    },
+
+    /**
+     * Creates the "No results found" message if it doesn't exist
+     */
+    createNoResultsFoundMessage() {
+        const noResultsMessage = document.querySelector('.no-results-message');
+        if (!noResultsMessage) {
+            const mainContainer = document.querySelector('.main-container');
+            const noResultsMessage = document.createElement('div');
+            noResultsMessage.className = 'no-results-message';
+            noResultsMessage.textContent = 'Whooops, no consumables found.';
+            mainContainer.appendChild(noResultsMessage);
+        } else {
+            noResultsMessage.style.display = 'block';
+        }
+    },
+
+    /**
+     * Hides the "No results found" message
+     */
+    hideNoResultsFoundMessage() {
+        const noResultsMessage = document.querySelector('.no-results-message');
+        noResultsMessage.style.display = 'none';
+    },
+
+    //----------------------------
+    //---- FUNCTIONS SORT BY -----
+    //----------------------------
+
+    /**
+     * Listen to the click on the sort-by button to display the options
+     */
+    listenToClickOnSortButton() {
+        const sortButton = document.querySelector('.sort-by-btn');
+        const sortOptionsElem = document.querySelector('.sort-options');
+    
+        sortButton.addEventListener('click', (event) => {
+            event.stopPropagation();
+            sortButton.classList.toggle('is-clicked');
+            sortOptionsElem.classList.toggle('hidden');
+        });
+    
+        document.addEventListener('click', () => {
+            sortButton.classList.remove('is-clicked');
+            sortOptionsElem.classList.add('hidden');
         });
     }
 };
