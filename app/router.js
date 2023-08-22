f5c48514bf6e3956b90b6e792c42879f284b44ed
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-08-22 16:41:10 +0200

    docs : commentaires router

--- app/router.js
+++ app/router.js
@@ -4,10 +4,10 @@ const generatorController = require("./controllers/generatorController.js");
 
 const router = new Router();
 
-router.get("/", mainController.renderHomePage); // "/" : **Home page**
-router.get("/weapons", mainController.renderWeaponsPage);
-router.get("/tools", mainController.renderToolsPage);
-router.get("/consumables", mainController.renderConsumablesPage);
-router.get("/generator", generatorController.renderGeneratorPage);
+router.get("/", mainController.renderHomePage); /**Home page**/
+router.get("/weapons", mainController.renderWeaponsPage); /**Weapons List**/
+router.get("/tools", mainController.renderToolsPage); /**Tools List**/
+router.get("/consumables", mainController.renderConsumablesPage); /**Consummables List**/
+router.get("/generator", generatorController.renderGeneratorPage); /**Generator Random Laodout**/
 
 module.exports = router;
