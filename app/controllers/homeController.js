6b0574d7ee9b290194d25993db0a6d7b9b77745c
Elisabeth FAUJOUR <efaujour@gmail.com>
2023-08-25 09:46:16 +0200

    feat: buttons home page

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -4,19 +4,19 @@ const consumables = require("../../data/consumables.json");
 
 const mainController = {
       renderHomePage(req, res) {
-      res.render("home", { pageTitle: "Hunt Loadout"});
+      res.render("home", { pageTitle: "Hunt Arsenal"});
     }, 
 
     renderWeaponsPage(req, res) {
-      res.render("weapons-list", { pageTitle: "Hunt Loadout - Arsenal Weapons", weapons });
+      res.render("weapons-list", { pageTitle: "Hunt Arsenal - Weapons", weapons });
     },
 
     renderToolsPage(req, res) {
-      res.render("tools-list", { pageTitle: "Hunt Loadout - Tools", tools });
+      res.render("tools-list", { pageTitle: "Hunt Arsenal - Tools", tools });
     },
     
     renderConsumablesPage(req, res) {
-      res.render("consumables-list", { pageTitle: "Hunt Loadout - Consumables", consumables });
+      res.render("consumables-list", { pageTitle: "Hunt Arsenal - Consumables", consumables });
     },
 
 };
